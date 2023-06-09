9115fa4d0691f06d93e46ecac2a700779dea89c9
Nathan Probert <nathanprobert@rogers.com>
2023-06-09 02:31:20 -0400

    hid download btn lol

---
+++ public/wordle.js
@@ -0,0 +1,10 @@
+// light/dark mode
+function dayNight() {
+    const toggleSwitch = document.querySelector(".toggle");
+    toggleSwitch.addEventListener("click", function(){
+        document.body.classList.toggle("switch");
+        this.classList.toggle("night");
+        this.classList.toggle("day");
+    })
+}
+dayNight();
