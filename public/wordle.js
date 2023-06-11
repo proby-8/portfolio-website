03a5acbca854fa8f2ae9e893a85d838ae280dae2
Nathan Probert <nathanprobert@rogers.com>
2023-06-10 23:14:43 -0400

    input mostly works, next add checks with enter etc

--- public/wordle.js
+++ public/wordle.js
@@ -8,3 +8,42 @@ function dayNight() {
     })
 }
 dayNight();
+
+// wordle stuff
+function input() {
+    document.getElementById(1).focus();
+
+    [...document.querySelectorAll(".element")].forEach(input => {
+        input.addEventListener("input", function(e) {
+            var id = this.id;
+
+            if ((id%10)<5){
+                if (this.value != ""){
+                    id++;
+                    document.getElementById(id).focus();
+                }
+            }
+        })
+    });
+
+    [...document.querySelectorAll(".element")].forEach(input => {
+        input.addEventListener("keydown", function(e) {
+            var id = this.id;
+
+            if (this.value == "" && ((id%10)>1) && (e.key == "Backspace" || e.key == "Delete")){
+                id--;
+                document.getElementById(id).focus();
+            }
+            else if (e.key == "ArrowLeft" && ((id%10)>1)) {
+                document.getElementById(--id).focus();
+            }
+            else if (e.key == "ArrowRight" && ((id%10)<5)) {
+                document.getElementById(++id).focus();
+            }
+            else if (this.value != "" && (e.keyCode >= 65 && e.keyCode <= 90)) {
+                this.value="";
+            }
+        })
+    });
+};
+input();
