f8941131bbe56ea597f41498313968f1c75745d5
Nathan Probert <nathanprobert@rogers.com>
2023-07-05 03:05:58 -0400

    updated keyboard so colour changes

--- public/wordle.js
+++ public/wordle.js
@@ -26,11 +26,11 @@ function input() {
         const buttons = document.querySelectorAll("button");
         buttons.forEach(button => {
             button.addEventListener('click', event => {
-                if (button.id == "enter") {
+                const activeElement = document.querySelector(".active");
+                var id = activeElement.id
+                var ogid = id;
 
-                    const activeElement = document.querySelector(".active");
-                    var id = activeElement.id
-                    var ogid = id;
+                if (button.id == "enter") {
 
                     // check if word is in file of all possible words
                     // return false if not, else continue
@@ -108,7 +108,26 @@ function input() {
                     }
                 }
                 else if (button.id == "delete") {
-
+                    const activeElement = document.querySelector(".active");
+                    if (activeElement.value == "" && ((id%10)>1)) {
+                        id--;
+                        document.getElementById(id).value = "";
+                        document.getElementById(id).focus();
+                        const allElements = document.querySelectorAll("*");
+                        allElements.forEach((element) => {
+                            element.classList.remove('active');
+                        });
+                        document.getElementById(id).classList.toggle("active");
+                    }
+                    else{
+                        document.getElementById(id).value = "";
+                        document.getElementById(id).focus();
+                        const allElements = document.querySelectorAll("*");
+                        allElements.forEach((element) => {
+                            element.classList.remove('active');
+                        });
+                        document.getElementById(id).classList.toggle("active");
+                    }
                 }
                 else{
                     const activeElement = document.querySelector(".active");
@@ -187,12 +206,17 @@ function input() {
 
                             if (attempt[i] == realWord[i]) {
                                 document.getElementById(curId).classList.add("green");
+                                document.getElementById(attempt[i].toUpperCase()).classList.add("green");
                             }
                             else if (realWord.includes(attempt[i])) {
                                 document.getElementById(curId).classList.add("yellow");
+                                document.getElementById(attempt[i].toUpperCase()).classList.add("yellow");
+
                             }
                             else {
                                 document.getElementById(curId).classList.add("grey");
+                                document.getElementById(attempt[i].toUpperCase()).classList.add("grey");
+
                             }
                         }
 
@@ -266,113 +290,3 @@ function input() {
     })
 };
 input();
-
-
-function keyboard() {
-    const buttons = document.querySelectorAll("button");
-    buttons.forEach(button => {
-        button.addEventListener('click', event => {
-            if (button.id == "enter") {
-
-                const activeElement = document.querySelector(".active");
-                var id = activeElement.id
-
-                // check if word is in file of all possible words
-                // return false if not, else continue
-                while ((id%5) != 0) {
-                    id++;
-                }
-
-                let attempt = "";
-                id=id-4;
-                for (var i=0; i<5; i++) {
-                    attempt += document.getElementById(id++).value;
-                }
-                attempt = attempt.toLowerCase();
-
-                if (attempt.length > 4 && words.includes(attempt)) {
-                    // real word
-                    console.log("Accepted input");
-                    for (var i=0; i<5; i++) {
-                        var curId = (parseInt(ogid) + parseInt(i) - 4);
-
-                        if (attempt[i] == realWord[i]) {
-                            document.getElementById(curId).classList.add("green");
-                        }
-                        else if (realWord.includes(attempt[i])) {
-                            document.getElementById(curId).classList.add("yellow");
-                        }
-                        else {
-                            document.getElementById(curId).classList.add("grey");
-                        }
-                    }
-
-                    // set row disabled, enable next row
-                    var newId = parseInt(ogid) + 6;
-                    var oldId = parseInt(ogid) - 4;
-                    for (var i=0; i<5; i++) {
-                        document.getElementById(oldId++).disabled = true;
-                    }
-                    if (newId > 51) {
-                        console.log("You Lost!");
-                    }
-                    if (attempt == realWord) {
-                        console.log("You Won!");
-                    }
-                    else{
-                        for (var i=0; i<5; i++) {
-                            document.getElementById(newId++).disabled = false;
-                        }
-                        newId=newId-5;
-                        const allElements = document.querySelectorAll("*");
-                        allElements.forEach((element) => {
-                            element.classList.remove('active');
-                        });
-                        document.getElementById(newId).focus();
-                        document.getElementById(newId).classList.toggle("active");
-                    }
-
-                }
-                else {
-                    // fake word
-                    document.getElementById(ogid).focus();
-
-                    // flash all box borders red
-                    id--;
-                    for (var i=0; i<5; i++) {
-                        document.getElementById(id--).classList.toggle("wrong");
-                    }
-
-                    // triggers after animation ends (1s)
-                    id++;
-                    setTimeout(function(){
-                        for (var i=0; i<5; i++) {
-                            document.getElementById(id++).classList.toggle("wrong");
-                        }
-                    },1000);
-                }
-            }
-            else if (button.id == "delete") {
-
-            }
-            else{
-                const activeElement = document.querySelector(".active");
-                var id = activeElement.id
-                activeElement.value=button.id;
-    
-                if ((id%10) < 5) {
-                    const allElements = document.querySelectorAll("*");
-                    allElements.forEach((element) => {
-                        element.classList.remove('active');
-                    });
-                    document.getElementById(++id).focus();
-                    document.getElementById(id).classList.toggle("active");
-                }
-                else {
-                    document.getElementById(id).focus();
-                }
-            }
-
-        });
-    });
-}
